deed9cc7acadde2c7b4db013eb1ad1a1f15da275
iceman832 <patrickc3832@gmail.com>
2015-08-25 22:21:24 +0200

    Commit changes made 2015/08/25 - Made changes to EventFile entity

---
+++ GlobalEventFileLog.Domain/EventFile/IEventFileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDDSkeleton.Common.Repository;
+
+namespace GlobalEventFileLog.Domain
+{
+    public interface IEventFileRepository: IRepository<EventFile, int>
+    {
+        IEnumerable<EventFile> FindAll();
+    }
+}
